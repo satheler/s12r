4a904076311bc15cd86db821efdfda35d66b739e
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2021-03-27 17:33:57 -0300

    fix: request url base in aws

--- aws/AWSHandler.ts
+++ aws/AWSHandler.ts
@@ -3,21 +3,18 @@ import { stringify } from 'querystring'
 import { IncomingMessage, ServerResponse } from 'http'
 import { CloudHandler, CloudRequest, CloudResponse } from 'core'
 import { ApiGatewayProxy, ApiGatewayResponse } from './ApiGatewayProxy'
+import { Socket } from 'net'
 
 const EVENT = 0
 const CALLBACK = 2
 
 export class AWSHandler implements CloudHandler {
-  private version!: string
   private isBase64Encoded!: boolean
 
-  public handle (params: CloudRequest<ApiGatewayProxy>): CloudResponse {
+  public handle(params: CloudRequest<ApiGatewayProxy>): CloudResponse {
     const event = params[EVENT]
     const callback = params[CALLBACK]
 
-    const { version = '1.0' } = event
-
-    this.version = version
     this.isBase64Encoded = process.env.BINARY_SUPPORT === 'yes'
 
     const request: IncomingMessage = this.request(event)
@@ -26,37 +23,24 @@ export class AWSHandler implements CloudHandler {
     return { request, response }
   }
 
-  private request (event: ApiGatewayProxy[0]): IncomingMessage {
-    const request = new Stream.Readable() as any
+  private request(event: ApiGatewayProxy[0]): IncomingMessage {
+    const request = new Stream.Readable() as IncomingMessage & { finished: true, getHeader:Function, getHeaders: Function }
 
-    const REMOVE_STAGE_PATTERN = new RegExp(`^/${event.requestContext.stage}(/)?`)
-
-    const urlByVersion = this.version === '2.0'
-      ? event.requestContext.http.path ?? event.rawPath ?? '/'
-      : event.requestContext.path ?? event.path ?? event.rawPath ?? '/'
-
-    request.url = urlByVersion.replace(REMOVE_STAGE_PATTERN, '/')
+    request.url = event?.path !== '' ? event.path : '/'
     request.finished = true
 
-    if (this.version === '1.0') {
-      if (event.multiValueQueryStringParameters) {
-        request.url = request.url.concat('?', stringify(event.multiValueQueryStringParameters))
-      }
-    } else {
-      if (event.rawQueryString) {
-        request.url = request.url.concat('?', event.rawQueryString)
-      }
+    if (event.multiValueQueryStringParameters) {
+      request.url = request.url!.concat('?', stringify(event.multiValueQueryStringParameters))
     }
 
-    request.method = this.version === '2.0' ? event.requestContext.http.method : event.httpMethod
+    request.method = event.httpMethod
     request.rawHeaders = []
     request.headers = {}
 
-    const headers = event.multiValueHeaders ?? event.headers ?? {}
+    const headers = event.multiValueHeaders ?? {}
 
     for (const key of Object.keys(headers)) {
-      const headerValues =
-        this.version === '1.0' ? headers[key] : headers[key].split(',')
+      const headerValues = headers[key]
       for (const value of headerValues) {
         request.rawHeaders.push(key)
         request.rawHeaders.push(value)
@@ -64,19 +48,9 @@ export class AWSHandler implements CloudHandler {
       request.headers[key.toLowerCase()] = headers[key].toString()
     }
 
-    if (this.version === '2.0') {
-      if (event?.cookies?.length) {
-        for (const value of event.cookies) {
-          request.rawHeaders.push('cookie')
-          request.rawHeaders.push(value)
-        }
-        request.headers.cookie = event.cookies.join(' ')
-      }
-    }
-
     request.getHeader = (name: string) => request.headers[name.toLowerCase()]
     request.getHeaders = () => request.headers
-    request.connection = {}
+    request.connection = {} as Socket
 
     if (event.body) {
       request.push(event.body, event.isBase64Encoded ? 'base64' : undefined)
@@ -86,15 +60,13 @@ export class AWSHandler implements CloudHandler {
     return request
   }
 
-  private response (callback: Function): ServerResponse {
+  private response(callback: Function): ServerResponse {
     const responseInitialValues: ApiGatewayResponse = {
       headers: {},
       multiValueHeaders: {},
       body: Buffer.from(''),
       isBase64Encoded: this.isBase64Encoded,
       statusCode: 200,
-      ...(this.version === '1.0' && { multiValueHeaders: {} }),
-      ...(this.version === '2.0' && { headers: {} })
     }
 
     const response = new Stream() as any
@@ -103,16 +75,16 @@ export class AWSHandler implements CloudHandler {
     response.statusCode = responseInitialValues.statusCode
 
     Object.defineProperty(response, 'statusCode', {
-      get () {
+      get() {
         return responseInitialValues.statusCode
       },
-      set (statusCode) {
+      set(statusCode) {
         responseInitialValues.statusCode = statusCode
       }
     })
 
     Object.defineProperty(response, 'headersSent', {
-      get () {
+      get() {
         return headersSent
       }
     })
@@ -154,11 +126,7 @@ export class AWSHandler implements CloudHandler {
         this.isBase64Encoded ? 'base64' : undefined
       )
 
-      if (this.version === '1.0') {
-        responseInitialValues.multiValueHeaders = response.headers
-      } else {
-        responseInitialValues.headers = response.headers
-      }
+      responseInitialValues.multiValueHeaders = response.headers
 
       response.writeHead(responseInitialValues.statusCode)
       this.fixApiGatewayHeaders(responseInitialValues)
@@ -168,37 +136,16 @@ export class AWSHandler implements CloudHandler {
     return response
   }
 
-  private fixApiGatewayHeaders (responseInitialValues: ApiGatewayResponse): void {
-    if (this.version === '1.0') {
-      const { multiValueHeaders } = responseInitialValues
+  private fixApiGatewayHeaders(responseInitialValues: ApiGatewayResponse): void {
+    const { multiValueHeaders } = responseInitialValues
 
-      if (!multiValueHeaders || Object.keys(multiValueHeaders).length === 0 || multiValueHeaders.constructor !== Object) {
-        return
-      }
-
-      for (const key of Object.keys(multiValueHeaders)) {
-        if (!Array.isArray(multiValueHeaders[key])) {
-          multiValueHeaders[key] = [multiValueHeaders[key] as string]
-        }
-      }
-
+    if (!multiValueHeaders || Object.keys(multiValueHeaders).length === 0 || multiValueHeaders.constructor !== Object) {
       return
     }
 
-    const cookies = responseInitialValues.headers?.['set-cookie']
-    if (cookies) {
-      responseInitialValues.cookies = Array.isArray(cookies) ? cookies : [cookies]
-      delete responseInitialValues.headers?.['set-cookie']
-    }
-
-    const { headers } = responseInitialValues
-    if (!headers || Object.keys(headers).length === 0 || headers.constructor !== Object) {
-      return
-    }
-
-    for (const key of Object.keys(headers)) {
-      if (Array.isArray(headers[key])) {
-        headers[key] = (headers[key] as string[]).join(',')
+    for (const key of Object.keys(multiValueHeaders)) {
+      if (!Array.isArray(multiValueHeaders[key])) {
+        multiValueHeaders[key] = [multiValueHeaders[key] as string]
       }
     }
   }
